e2a0e0d1278a3481f084365727909bb1d420b3b3
erickferrareto <e.ferraretowork@gmail.com>
2022-09-11 16:27:53 -0300

    working with dinamic recycler view

--- app/src/main/java/com/example/testandorecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/testandorecyclerview/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,29 +17,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var listTest = mutableListOf<MainItem>()
+        listTest.add(
+            MainItem(
+                image = R.drawable.ic_baseline_brightness_3_24,
+                textTest = R.string.olá
+            )
+        )
+        listTest.add(
+            MainItem(
+                image = R.drawable.ic_baseline_brightness_high_24,
+                textTest = R.string.mundo
+            )
+        )
+
         recyclerView = findViewById(R.id.reciycler)
-        val adapter = recyclerAdapter()
+        val adapter = recyclerAdapter(listTest)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
 
     }
-    private inner class recyclerAdapter : RecyclerView.Adapter<recyclerViewHolder>() {
+    private inner class recyclerAdapter(private val listTest : List<MainItem>) : RecyclerView.Adapter<recyclerViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): recyclerViewHolder {
             val inflate = layoutInflater.inflate(R.layout.recycler, parent, false)
             return recyclerViewHolder(inflate)
         }
 
         override fun onBindViewHolder(holder: recyclerViewHolder, position: Int) {
-
+            val itemCorrent = listTest[position]
+            holder.bind(itemCorrent)
         }
 
         override fun getItemCount(): Int {
-            return 15
+            return listTest.size
         }
 
     }
-    private inner class recyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private inner class recyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(View: MainItem){
+            val textTest : TextView  = itemView.findViewById(R.id.texto)
+            val imageTest : ImageView = itemView.findViewById(R.id.image)
+            imageTest.setImageResource(View.image)
+            textTest.setText(View.textTest)
 
+        }
     }
 }
---
+++ app/src/main/java/com/example/testandorecyclerview/MainItem.kt
@@ -0,0 +1,13 @@
+package com.example.testandorecyclerview
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class MainItem(
+
+    @DrawableRes var image : Int,
+    @StringRes var textTest : Int
+
+    )
